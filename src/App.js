1ce1762aa331267f64a0f98054e1f86ec17b9c51
Susy2801 <nduy200@gmail.com>
2024-03-11 11:13:48 +0700

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [tvl, setTvl] = useState("");
+  const [result, setResult] = useState("");
+  const [des, setDes] = useState("");
+  const [name, setName] = useState("");
+  const [ava, setAva] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingAva, setLoadingAva] = useState(false);
+
+  useEffect(() => {
+    if (tvl.trim() === "") {
+      setResult("");
+      return;
+    }
+
+    async function getTVL() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.llama.fi/tvl/${tvl
+            .trim()
+            .toLowerCase()
+            .replace(/\s/g, "-")}`
+        );
+        if (response.status === 200) {
+          const data = await response.json();
+          setResult(data);
+        }
+      } catch (error) {
+        console.error("Error fetching TVL:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    async function getAva() {
+      try {
+        setLoadingAva(true);
+        const response = await fetch(
+          `https://api.llama.fi/protocol/${tvl
+            .trim()
+            .toLowerCase()
+            .replace(/\s/g, "-")}`
+        );
+        if (response.status === 200) {
+          const data = await response.json();
+          setAva(data.logo);
+          setDes(data.description);
+          setName(data.name);
+          console.log(data);
+        } else {
+          setAva("");
+        }
+      } catch (error) {
+        console.error("Error fetching TVL:", error);
+      } finally {
+        setLoadingAva(false);
+      }
+    }
+    getAva();
+    getTVL();
+  }, [tvl]);
+
+  const handleChange = (e) => {
+    setTvl(e.target.value);
+  };
+
+  const handleClick = () => {
+    setTvl(tvl);
+  };
+
+  const handleClear = () => {
+    setTvl("");
+    setAva("");
+    setDes("");
+    setName("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={tvl} onChange={handleChange} />
+      <button onClick={handleClick}>Send</button>
+      <div className="header">
+        <h2>Total Locked Value</h2>
+        {loading && (
+          <div className="loading">
+            <div class="loader"></div>
+          </div>
+        )}
+        {result && <strong>$</strong>}
+        {result ? Math.floor(result).toLocaleString() : <div>No data</div>}
+      </div>
+      <button onClick={handleClear}>Clear</button>
+      <div>
+        {loadingAva ? (
+          <div className="loading">
+            {" "}
+            <div class="loader"></div>
+          </div>
+        ) : (
+          ava && <img className="ava" src={ava} alt="" />
+        )}
+      </div>
+      <div className="name">{name}</div>
+      <div className="des">{des}</div>
     </div>
   );
 }
